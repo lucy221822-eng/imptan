01c8553481f0a99fc1c6289fb2a7b1c49fa8ea52
lucy221822-eng <lucy221822@gmail.com>
2026-04-23 20:27:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+if (menuBtn && mobileMenu) {
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+
+  mobileMenu.querySelectorAll("a").forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenu.classList.add("hidden");
+    });
+  });
+}
